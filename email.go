3e416af476cff8fbac61bfd23988c5c2922f41da
Kakysha <ezhikvdele@gmail.com>
2018-01-24 15:12:24 +0300

    custom headers support

--- email.go
+++ email.go
@@ -2,39 +2,40 @@ package elasticemail
 
 import (
 	"context"
+	"encoding/json"
 )
 
 // Email is the JSON structure accepted by ElasticEmail Email API.
 // fields list from https://api.elasticemail.com/public/help#Email_Send
 type Email struct {
-	BodyHTML          string       `json:",omitempty"`
-	BodyText          string       `json:",omitempty"`
-	Channel           string       `json:",omitempty"`
-	Charset           string       `json:",omitempty"`
-	CharsetBodyHTML   string       `json:",omitempty"`
-	CharsetBodyText   string       `json:",omitempty"`
-	EncodingType      encodingType `json:",omitempty"`
-	From              string       `json:",omitempty"`
-	FromName          string       `json:",omitempty"`
-	IsTransactional   bool         `json:",omitempty"`
-	Lists             string       `json:",omitempty"`
-	Merge             string       `json:",omitempty"`
-	MsgBCC            string       `json:",omitempty"`
-	MsgCC             string       `json:",omitempty"`
-	MsgFrom           string       `json:",omitempty"`
-	MsgFromName       string       `json:",omitempty"`
-	MsgTo             string       `json:",omitempty"`
-	PoolName          string       `json:",omitempty"`
-	PostBack          string       `json:",omitempty"`
-	ReplyTo           string       `json:",omitempty"`
-	ReplyToName       string       `json:",omitempty"`
-	Segments          string       `json:",omitempty"`
-	Sender            string       `json:",omitempty"`
-	SenderName        string       `json:",omitempty"`
-	Subject           string       `json:",omitempty"`
-	Template          string       `json:",omitempty"`
-	TimeOffSetMinutes string       `json:",omitempty"`
-	To                string       `json:",omitempty"`
+	BodyHTML          string            `json:",omitempty"`
+	BodyText          string            `json:",omitempty"`
+	Channel           string            `json:",omitempty"`
+	Charset           string            `json:",omitempty"`
+	CharsetBodyHTML   string            `json:",omitempty"`
+	CharsetBodyText   string            `json:",omitempty"`
+	EncodingType      encodingType      `json:",omitempty"`
+	From              string            `json:",omitempty"`
+	FromName          string            `json:",omitempty"`
+	IsTransactional   bool              `json:",omitempty"`
+	Lists             string            `json:",omitempty"`
+	Merge             string            `json:",omitempty"`
+	MsgBCC            string            `json:",omitempty"`
+	MsgCC             string            `json:",omitempty"`
+	MsgFrom           string            `json:",omitempty"`
+	MsgFromName       string            `json:",omitempty"`
+	MsgTo             string            `json:",omitempty"`
+	PoolName          string            `json:",omitempty"`
+	ReplyTo           string            `json:",omitempty"`
+	ReplyToName       string            `json:",omitempty"`
+	Segments          string            `json:",omitempty"`
+	Sender            string            `json:",omitempty"`
+	SenderName        string            `json:",omitempty"`
+	Subject           string            `json:",omitempty"`
+	Template          string            `json:",omitempty"`
+	TimeOffSetMinutes string            `json:",omitempty"`
+	To                string            `json:",omitempty"`
+	CustomHeaders     map[string]string `json:"-"` // postback headers are presented in email headers also
 }
 
 type encodingType byte
@@ -59,7 +60,14 @@ func (c *Client) Send(e *Email) *Response {
 
 // SendContext is the same as Send, and it allows the caller to pass in a context
 func (c *Client) SendContext(ctx context.Context, e *Email) *Response {
-	return c.HTTPPost(ctx, "email/send", e)
+	jsonBytes, _ := json.Marshal(e)
+	var rawParams map[string]string
+	json.Unmarshal(jsonBytes, &rawParams)
+
+	for k, v := range e.CustomHeaders {
+		rawParams[("headers_postback-" + k)] = "postback-" + k + ": " + v
+	}
+	return c.HTTPPost(ctx, "email/send", rawParams)
 }
 
 // Status retrieves detailed status of a unique email sent through your account. Returns a 'Email has expired and the status is unknown.' error, if the email has not been fully processed yet.
